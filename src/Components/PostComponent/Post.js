0648ce714279ce400552aee1287fc0fbad21bdff
AdGWE <adrielgohsg@gmail.com>
2022-01-21 19:44:20 +0800

    Home page css changes, Changed pagination buttons and edited channel css

--- src/Components/PostComponent/Post.js
+++ src/Components/PostComponent/Post.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Post.css";
 import parseTime from "../../helperFunctions/parseTime";
 const Post = (props) => {
 	// Takes in a prop of "Posts" as an array
@@ -20,24 +21,28 @@ const Post = (props) => {
 					<div className="row g-0">
 						<div className="col-1">Upvote</div>
 						<div className="col-9 bg-white p-2 position-relative">
-							<div className="d-flex flex-row align-items-baseline">
-								<h6 className="d-inline"><a href={data.fk_subforum_id} className="text-dark text-decoration-none">{data.Subforum.subforum_name}</a></h6>
-							</div>
-							<div className="d-flex flex-row align-items-baseline">
-								<span className="Add Profile Picture Here"></span><p className="d-inline text-secondary me-1">Posted by <a>{data.User.first_name}</a></p>
-								<p className="d-inline text-secondary clickable-link" id={"post_" + data.fk_user_id}></p>
-								<p className="fw-light text-secondary mx-1">•</p>
-								<p className="text-secondary" id={"post_" + data.post_id + "_time"}>{parseTime(data.post_created_at)}</p>
-							</div>
 							<a style={{ textDecoration: "none" }} href={data.fk_subforum_id + "/" + data.post_id} className="d-flex flex-row">
 								<h5 style={{ color: "black" }} id={"post_" + data.post_id + "_content"} className="mb-0">{data.post_title}</h5>
 								<div className="d-flex flex-row">
 
 								</div>
 							</a>
+							<div className="d-flex flex-row align-items-baseline">
+								<span className="Add Profile Picture Here"></span><p className="d-inline text-secondary me-1">Posted by <a>{data.User.first_name}</a></p>
+								<p className="d-inline text-secondary clickable-link" id={"post_" + data.fk_user_id}></p>
+								<p className="fw-light text-secondary mx-1">•</p>
+								<p className="text-secondary" id={"post_" + data.post_id + "_time"}>{parseTime(data.post_created_at)}</p>
+							</div>
 							<p className="mt-2 PostBody">{data.post_content}</p>
 							<div id={"post_media_" + data.post_id} className="d-flex flex-row justify-content-center mediaDiv">  </div>
-							<div className="toolbar d-flex flex-row align-items-center mt-2">
+							<div className="d-flex flex-row align-items-baseline">
+								<h6 className="Tags">
+									<a href={data.fk_subforum_id} className="text-dark text-decoration-none">{data.Subforum.subforum_name}</a>
+								</h6>
+								<p className="fw-light text-secondary mx-1">•</p>
+								<h6 className="Tags">
+									<a href={data.fk_subforum_id} className="text-dark text-decoration-none">{data.Grade.grade_name}</a>
+								</h6>
 							</div>
 						</div>
 						<div className="col-lg-2">Answers</div>
